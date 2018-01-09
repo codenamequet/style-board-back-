d6b019ea1bef57ec525e29ac830fb2483f46eca4
Your Name <you@example.com>
2018-01-09 14:16:11 -0500

    correct connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,10 +5,10 @@ mongoose.connect("mongodb://localhost/pdf", { useMongoClient: true }) //need to 
 if (process.env.NODE_ENV == "production") {
     mongoose.connect(process.env.MLAB_URL)
   } else {
-    mongoose.connect("mongodb://localhost/pdf");
+    mongoose.connect("mongodb://localhost/pdf")
   }
 
 mongoose.Promise = Promise
 
 module.exports = mongoose
-mongodb://p2admin:project2@ds249787.mlab.com:49787/clothes
+
