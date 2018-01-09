e1d5e3a3fe4bd43094701c97ef3f31074f68e649
Your Name <you@example.com>
2018-01-09 14:11:58 -0500

    seeding cloud db

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost/pdf", { useMongoClient: true })
+mongoose.connect("mongodb://localhost/pdf", { useMongoClient: true }) //need to change the database name
+
+if (process.env.NODE_ENV == "production") {
+    mongoose.connect(process.env.MLAB_URL)
+  } else {
+    mongoose.connect("mongodb://localhost/pdf");
+  }
 
 mongoose.Promise = Promise
 
 module.exports = mongoose
+mongodb://p2admin:project2@ds249787.mlab.com:49787/clothes
